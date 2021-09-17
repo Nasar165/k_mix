ada5d72e800e7ec06598471f4980dcc97017919b
Nasar Eddaoui <nasar165@hotmail.com>
2021-09-17 16:53:16 +0700

    minor fixes

--- simple_node_server/main.js
+++ simple_node_server/main.js
@@ -1,12 +1,17 @@
 const express = require("express");
 require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require('body-parser')
+
 const httpServer = express();
 
 httpServer.use('/', cors())
+httpServer.use('/', bodyParser.json())
+httpServer.use('/', bodyParser.urlencoded({ extended: true }))
 
 httpServer.get("/test",function (_req,res){
-    res.send("hello world")
+    res.setHeader("Content-Type","application/json")
+    res.status(200).send('{"data":"hello world"}')
 })
 
 const port = process.env.PORT || 8080
